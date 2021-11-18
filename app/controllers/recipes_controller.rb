3efd690b822d1ecf635354b4707579f3abed2b97
Dex Fitch <fitch.dexter@gmail.com>
2021-11-17 20:57:40 -0500

    Adding controllers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,44 @@
 class RecipesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :recipe_data_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :recipe_data_not_found
+
+    def index
+        render json: Recipe.all, status: :ok
+    end
+
+    def show
+        render json: Recipe.find(session[:recipe_id]), status: :ok
+    end
+    
+    def create
+        new_recipe = Recipe.create!(recipe_params)
+        session[:recipe_id] ||= new_recipe.id
+        render json: new_recipe, status: :created
+    end
+
+    def update
+        this_recipe = Recipe.find(params[:id])
+        this_recipe.update!(recipe_params)
+        render json: this_recipe, status: :ok
+    end
+
+    def destroy
+        this_recipe = Recipe.find(params[:id])
+        this_recipe.destroy
+        render status: :no_content
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:name, :instructions, :url, :season, :user_id, :recipe)
+    end
+
+    def recipe_data_invalid(error_hash)
+        render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def recipe_data_not_found
+        render json: { error: "Recipe not found, please try again" }, status: :not_found
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :user_data_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :user_data_not_found
+
+    def index
+        render json: User.all, status: :ok
+    end
+
+    def show
+        render json: User.find(session[:user_id]), status: :ok
+    end
+    
+    def create
+        new_user = User.create!(user_params)
+        session[:user_id] ||= new_user.id
+        render json: new_user, status: :created
+    end
+
+    def update
+        this_user = User.find(params[:id])
+        this_user.update!(user_params)
+        render json: this_user, status: :ok
+    end
+
+    def destroy
+        this_user = User.find(params[:id])
+        this_user.destroy
+        render status: :no_content
+    end
+
+    private
+    
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :first_name, :last_name, :nickname, :email, :linkedin, :slack, :url, :user)
+    end
+
+    def user_data_invalid(error_hash)
+        render json: { errors: error_hash.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def user_data_not_found
+        render json: { error: "User not found, please try again" }, status: :not_found
+    end
 end
